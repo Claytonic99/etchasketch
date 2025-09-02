208ab790ced02fbf5259ed0d7efb0f22d60366a0
Clayton <clay10bg@gmail.com>
2025-09-02 12:13:57 -0600

    Remove Old Squares When Creating New Squares
    Add functionality of choose size button to remove all previous squares
    when creating new squares.

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const grid = document.querySelector("#container");
-let square = null;
+const button = document.querySelector("#button");
+button.addEventListener("click", () => {
+    // Remove previous squares
+    const bad = document.querySelectorAll("#square");
+    bad.forEach(sq => sq.remove());
+    // Prompt user for new number of squares
+    let size = window.prompt("Enter a size from 1 - 100");
+    // Create new squares
+    if (size > 0 && size <= 100)
+    {
+        for (let i = 0; i < size; i++)
+        {
+            console.log(i);
+            let myString = "div"+i;
+            let square = document.createElement(myString);
+            square.textContent = i;
+            square.className = "square";
+            square.id = "square";
+            square.style.backgroundColor = "white";
+            console.log(square.textContent);
+            square.addEventListener('mouseenter', () => {
+                square.style.backgroundColor = 'black';
+            });
+            
+            grid.appendChild(square);
+        }
+    }
+});
 
-for (let i = 0; i < 16; i++)
-{
-    console.log(i);
-    let myString = "div"+i;
-    square = document.createElement(myString);
-    square.textContent = i;
-    square.className = "square";
-    console.log(square.textContent);
-    
-    grid.appendChild(square);
-}
