1496c817eebae83d114493e4b7f0c5ae9e18103f
Clayton <clay10bg@gmail.com>
2025-09-02 13:33:24 -0600

    Correct Grid Size and Use Random Colors
    Corrected the grid size by setting the container width and height to
    the same value (800) and then calculating square width and height as
    800 / size when 'choose size' button is clicked. The grid is also
    squared whatever value is given (between 1 and 100). If the user enters
    20, the grid will be 20 x 20 (400). The random color bit is for extra
    credit. I tried to do a random rgb value, but found it was easier to
    use a random hex color assignemnt.

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 const grid = document.querySelector("#container");
 const button = document.querySelector("#button");
+let opacity = 0;
 button.addEventListener("click", () => {
     // Remove previous squares
     const bad = document.querySelectorAll("#square");
     bad.forEach(sq => sq.remove());
+    // grid.innerHTML = ""; //also works
+
     // Prompt user for new number of squares
     let size = window.prompt("Enter a size from 1 - 100");
+    
     // Create new squares
     if (size > 0 && size <= 100)
     {
-        for (let i = 0; i < size; i++)
+        for (let i = 0; i < size * size; i++)
         {
             console.log(i);
-            let myString = "div"+i;
+            let myString = "div";
             let square = document.createElement(myString);
-            square.textContent = i;
+            square.style.width = (800 / size) + "px";
+            square.style.height = (800 / size) + "px";
             square.className = "square";
             square.id = "square";
+            // square.textContent = i;
             square.style.backgroundColor = "white";
-            console.log(square.textContent);
             square.addEventListener('mouseenter', () => {
-                square.style.backgroundColor = 'black';
+                // square.style.backgroundColor = 'black';
+                const hexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+                let color = "#";
+                for (let z = 0; z < 6; z++)
+                {
+                    const random = Math.floor(Math.random()*16);
+                    color += hexCharacters[random];
+                }
+                console.log("Your color is:"+color);
+                square.style.backgroundColor = color;
+                opacity += 0.1;
+                square.style.opacity = opacity;
+                
             });
             
             grid.appendChild(square);
